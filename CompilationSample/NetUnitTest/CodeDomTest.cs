11d94034111e49b46c6547a3aba4a740df48b58f
Keiho Sakapon <git@saka-pon.net>
2019-03-08 13:37:19 +0900

    Add test for CodeDomProvider

---
+++ CompilationSample/NetUnitTest/CodeDomTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NetUnitTest
+{
+    [TestClass]
+    public class CodeDomTest
+    {
+        [TestMethod]
+        public void Add_1()
+        {
+            var source = @"
+public static class Numerics
+{
+public static double Add(int x, double y) { return x + y; }
+}";
+            var provider = CodeDomProvider.CreateProvider("CSharp");
+            var results = provider.CompileAssemblyFromSource(new CompilerParameters { GenerateInMemory = true }, source);
+
+            var add = results.CompiledAssembly.ExportedTypes.First().GetMethod("Add");
+            Assert.AreEqual(2.3, add.Invoke(null, new object[] { 1, 1.3 }));
+        }
+    }
+}
