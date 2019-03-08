0445bb8608fe87093c8f2e664f4e62840260e4c9
Keiho Sakapon <git@saka-pon.net>
2019-03-08 14:25:07 +0900

    Add test

--- CompilationSample/NetUnitTest/CodeDomTest.cs
+++ CompilationSample/NetUnitTest/CodeDomTest.cs
@@ -17,11 +17,50 @@ public static class Numerics
 {
 public static double Add(int x, double y) { return x + y; }
 }";
+            var options = new CompilerParameters()
+            {
+                GenerateInMemory = true,
+            };
             var provider = CodeDomProvider.CreateProvider("CSharp");
-            var results = provider.CompileAssemblyFromSource(new CompilerParameters { GenerateInMemory = true }, source);
+            var results = provider.CompileAssemblyFromSource(options, source);
 
             var add = results.CompiledAssembly.ExportedTypes.First().GetMethod("Add");
             Assert.AreEqual(2.3, add.Invoke(null, new object[] { 1, 1.3 }));
+        }
+
+        [TestMethod]
+        public void Add_2()
+        {
+            var expression = "z += x + y";
+            var vars = new Dictionary<string, object>
+            {
+                { "x", 1 },
+                { "y", 1.3 },
+                { "z", 3.1 },
+            };
+
+            var source = $@"
+public static class Numerics
+{{
+public static void Add(ref int x, ref double y, ref double z)
+{{
+{expression};
+}}
+}}";
+            // この場合は参照なしでもコンパイルできます。
+            var options = new CompilerParameters(new[] { "System.dll", "System.Core.dll" })
+            {
+                GenerateInMemory = true,
+            };
+            var provider = CodeDomProvider.CreateProvider("CSharp");
+            var results = provider.CompileAssemblyFromSource(options, source);
+
+            var add = results.CompiledAssembly.ExportedTypes.First().GetMethod("Add");
+            var args = vars.Values.ToArray();
+            add.Invoke(null, args);
+            Assert.AreEqual(5.4, args[2]);
+            add.Invoke(null, args);
+            Assert.AreEqual(7.7, args[2]);
         }
     }
 }
