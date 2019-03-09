07353063f721d7d8cd840e34a9bd9179219b26e0
Keiho Sakapon <git@saka-pon.net>
2019-03-09 23:06:01 +0900

    Add test for syntax to lambda

--- CompilationSample/UnitTest/SyntaxLambda/SyntaxLambdaHelper.cs
+++ CompilationSample/UnitTest/SyntaxLambda/SyntaxLambdaHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -9,6 +10,42 @@ namespace UnitTest.SyntaxLambda
 {
     public static class SyntaxLambdaHelper
     {
+        public static void ExecuteExpression(string expression, Dictionary<string, object> vars)
+        {
+            var assignment = GetAssignment(expression);
+
+            var left = (IdentifierNameSyntax)assignment.Left;
+            var leftVarName = left.Identifier.ValueText;
+
+            var rightVars = assignment.Right.DescendantNodesAndSelf()
+                .OfType<IdentifierNameSyntax>()
+                .Select(n => n.Identifier.ValueText)
+                .Distinct()
+                .ToDictionary(n => n, n => Variable(vars[n].GetType(), n));
+
+            var body = assignment.Right.ToExpression(rightVars);
+            var lambda = Lambda(body, rightVars.Values);
+            Console.WriteLine(lambda);
+            var func = lambda.Compile();
+            vars[leftVarName] = func.DynamicInvoke(rightVars.Keys.Select(n => vars[n]).ToArray());
+        }
+
+        public static AssignmentExpressionSyntax GetAssignment(string expression)
+        {
+            var method = (MethodDeclarationSyntax)ParseMember($"void Action_0() => {expression};");
+            return (AssignmentExpressionSyntax)method.ExpressionBody.Expression;
+        }
+
+        public static MemberDeclarationSyntax ParseMember(string text)
+        {
+            var tree = CSharpSyntaxTree.ParseText(text);
+            var diagnostics = tree.GetDiagnostics().ToArray();
+            if (diagnostics.Length > 0) return null;
+
+            var root = tree.GetCompilationUnitRoot();
+            return root.Members[0];
+        }
+
         public static Expression ToExpression(this ExpressionSyntax syntax, Dictionary<string, ParameterExpression> vars)
         {
             var kind = syntax.Kind();
---
+++ CompilationSample/UnitTest/SyntaxLambda/SyntaxLambdaTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.SyntaxLambda
+{
+    [TestClass]
+    public class SyntaxLambdaTest
+    {
+        [TestMethod]
+        public void Add_1()
+        {
+            var expression = "z = x + y";
+            var vars = new Dictionary<string, object>
+            {
+                { "x", 1.0 },
+                { "y", 1.3 },
+            };
+            SyntaxLambdaHelper.ExecuteExpression(expression, vars);
+            Assert.AreEqual(2.3, vars["z"]);
+        }
+
+        [TestMethod]
+        public void Add_2()
+        {
+            var expression = "z = (double)x + y";
+            var vars = new Dictionary<string, object>
+            {
+                { "x", 1 },
+                { "y", 1.3 },
+            };
+            SyntaxLambdaHelper.ExecuteExpression(expression, vars);
+            Assert.AreEqual(2.3, vars["z"]);
+        }
+
+        [TestMethod]
+        public void Mean_1()
+        {
+            var expression = "z = (x + y) / 2";
+            var vars = new Dictionary<string, object>
+            {
+                { "x", 1.0 },
+                { "y", 1.3 },
+            };
+            SyntaxLambdaHelper.ExecuteExpression(expression, vars);
+            Assert.AreEqual(1.15, vars["z"]);
+        }
+
+        [TestMethod]
+        public void Mean_2()
+        {
+            var expression = "z = (x + y) / 2.0";
+            var vars = new Dictionary<string, object>
+            {
+                { "x", 1 },
+                { "y", 2 },
+            };
+            SyntaxLambdaHelper.ExecuteExpression(expression, vars);
+            Assert.AreEqual(1.5, vars["z"]);
+        }
+    }
+}
