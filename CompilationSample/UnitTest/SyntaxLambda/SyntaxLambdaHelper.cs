d517ddf7ccdc487e43e09b7af25b5e43df311e90
Keiho Sakapon <git@saka-pon.net>
2019-03-10 20:36:27 +0900

    Use script compilation

--- CompilationSample/UnitTest/SyntaxLambda/SyntaxLambdaHelper.cs
+++ CompilationSample/UnitTest/SyntaxLambda/SyntaxLambdaHelper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using static System.Linq.Expressions.Expression;
@@ -10,24 +11,45 @@ namespace UnitTest.SyntaxLambda
 {
     public static class SyntaxLambdaHelper
     {
-        public static void ExecuteExpression(string expression, Dictionary<string, object> vars)
+        public static void ExecuteExpression(string expression, Dictionary<string, object> variables)
         {
             var assignment = (AssignmentExpressionSyntax)SyntaxFactory.ParseExpression(expression);
 
             var left = (IdentifierNameSyntax)assignment.Left;
             var leftVarName = left.Identifier.ValueText;
 
-            var rightVars = assignment.Right.DescendantNodesAndSelf()
+            var context = assignment.Right.ToModel(variables);
+
+            var body = context.RootExpression.ToExpression(context.MetaVariables);
+            var lambda = Lambda(body, context.MetaVariables.Values);
+            Console.WriteLine(lambda);
+            var func = lambda.Compile();
+            variables[leftVarName] = func.DynamicInvoke(context.MetaVariables.Keys.Select(n => variables[n]).ToArray());
+        }
+
+        static SyntaxContext ToModel(this ExpressionSyntax sourceExpression, Dictionary<string, object> variables)
+        {
+            var metaVars = sourceExpression.DescendantNodesAndSelf()
                 .OfType<IdentifierNameSyntax>()
                 .Select(n => n.Identifier.ValueText)
                 .Distinct()
-                .ToDictionary(n => n, n => Variable(vars[n].GetType(), n));
+                .ToDictionary(n => n, n => Variable(variables[n].GetType(), n));
 
-            var body = assignment.Right.ToExpression(rightVars);
-            var lambda = Lambda(body, rightVars.Values);
-            Console.WriteLine(lambda);
-            var func = lambda.Compile();
-            vars[leftVarName] = func.DynamicInvoke(rightVars.Keys.Select(n => vars[n]).ToArray());
+            var script_vars = string.Join("\r\n", metaVars.Values.Select(v => $"var {v.Name} = default({v.Type});"));
+            var script = $"{script_vars}\r\n{sourceExpression}";
+            var tree = SyntaxFactory.ParseSyntaxTree(script, CSharpParseOptions.Default.CommonWithKind(SourceCodeKind.Script));
+            var compilation = CSharpCompilation.CreateScriptCompilation("CompilationSample", tree, new[] { MetadataReference.CreateFromFile(typeof(object).Assembly.Location) });
+            var semanticModel = compilation.GetSemanticModel(tree);
+
+            var root = tree.GetCompilationUnitRoot();
+            var rootExpression = root.Members.Last().DescendantNodes().OfType<ExpressionSyntax>().First();
+
+            return new SyntaxContext
+            {
+                RootExpression = rootExpression,
+                SemanticModel = semanticModel,
+                MetaVariables = metaVars,
+            };
         }
 
         public static Expression ToExpression(this ExpressionSyntax syntax, Dictionary<string, ParameterExpression> vars)
@@ -66,6 +88,18 @@ namespace UnitTest.SyntaxLambda
                 default:
                     throw new NotSupportedException();
             }
+        }
+    }
+
+    public class SyntaxContext
+    {
+        public ExpressionSyntax RootExpression { get; set; }
+        public SemanticModel SemanticModel { get; set; }
+        public Dictionary<string, ParameterExpression> MetaVariables { get; set; }
+
+        public ISymbol GetSymbol(ExpressionSyntax syntax)
+        {
+            return SemanticModel.GetSymbolInfo(syntax).Symbol;
         }
     }
 }
