fb4db7b7b4f446ecdecf7d2d34f659c56715d502
Keiho Sakapon <git@saka-pon.net>
2019-08-06 16:11:09 +0900

    Fix tab

--- AtCoderSample/ABC136Console/C.cs
+++ AtCoderSample/ABC136Console/C.cs
@@ -3,21 +3,21 @@ using System.Linq;
 
 class C
 {
-    static void Main()
-    {
-        Console.ReadLine();
-        var a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+	static void Main()
+	{
+		Console.ReadLine();
+		var a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
 
-        for (var i = a.Length - 2; i >= 0; i--)
-        {
-            var d = a[i] - a[i + 1];
-            if (d > 1)
-            {
-                Console.WriteLine("No");
-                return;
-            }
-            if (d == 1) a[i]--;
-        }
-        Console.WriteLine("Yes");
-    }
+		for (var i = a.Length - 2; i >= 0; i--)
+		{
+			var d = a[i] - a[i + 1];
+			if (d > 1)
+			{
+				Console.WriteLine("No");
+				return;
+			}
+			if (d == 1) a[i]--;
+		}
+		Console.WriteLine("Yes");
+	}
 }
