0d2f4aa301dd18269c080186936bcf192304588e
Keiho Sakapon <git@saka-pon.net>
2019-08-07 22:50:53 +0900

    Update template

--- AtCoderSample/ABC000/A.cs
+++ AtCoderSample/ABC000/A.cs
@@ -3,10 +3,10 @@ using System.Linq;
 
 class A
 {
-    static void Main()
-    {
-        Console.ReadLine();
-        var n = int.Parse(Console.ReadLine());
-        var a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-    }
+	static void Main()
+	{
+		Console.ReadLine();
+		var n = int.Parse(Console.ReadLine());
+		var a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+	}
 }
--- AtCoderSample/ABC000/D.cs
+++ AtCoderSample/ABC000/D.cs
@@ -3,10 +3,10 @@ using System.Linq;
 
 class D
 {
-    static void Main()
-    {
-        Console.ReadLine();
-        var n = int.Parse(Console.ReadLine());
-        var a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-    }
+	static void Main()
+	{
+		Console.ReadLine();
+		var n = int.Parse(Console.ReadLine());
+		var a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+	}
 }
