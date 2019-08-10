46c2e90e6e1cb6c9cfb21a9d60836ef098f6e730
Keiho Sakapon <git@saka-pon.net>
2019-08-10 23:49:47 +0900

    Improve code

--- AtCoderSample/ABC137/C.cs
+++ AtCoderSample/ABC137/C.cs
@@ -3,10 +3,5 @@ using System.Linq;
 
 class C
 {
-    static void Main()
-    {
-        var n = int.Parse(Console.ReadLine());
-        var a = Enumerable.Range(0, n).Select(i => new string(Console.ReadLine().OrderBy(s => s).ToArray())).GroupBy(s => s).Where(g => g.Count() > 1).Select(g => g.Count()).Sum(c => (long)c * (c - 1) / 2);
-        Console.WriteLine(a);
-    }
+	static void Main() => Console.WriteLine(Enumerable.Range(0, int.Parse(Console.ReadLine())).Select(i => new string(Console.ReadLine().OrderBy(s => s).ToArray())).GroupBy(s => s).Select(g => g.Count()).Where(c => c > 1).Sum(c => (long)c * (c - 1) / 2));
 }
