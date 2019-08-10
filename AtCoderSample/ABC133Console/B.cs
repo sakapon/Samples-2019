cff717a047af802fbcef3fa4c7fd8f8c2ecde7e6
Keiho Sakapon <git@saka-pon.net>
2019-08-10 16:48:33 +0900

    Write Enumerable

--- AtCoderSample/ABC133Console/B.cs
+++ AtCoderSample/ABC133Console/B.cs
@@ -1,18 +1,17 @@
 ﻿using System;
 using System.Linq;
-using static System.Linq.Enumerable;
 
 class B
 {
     static void Main()
     {
         var a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-        var ps = Range(0, a[0]).Select(i => Console.ReadLine().Split(' ').Select(int.Parse).ToArray()).ToArray();
+        var ps = Enumerable.Range(0, a[0]).Select(i => Console.ReadLine().Split(' ').Select(int.Parse).ToArray()).ToArray();
 
-        var c = Range(0, a[0]).SelectMany(i => Range(i + 1, a[0] - i - 1).Select(j => new { i, j }))
+        var c = Enumerable.Range(0, a[0]).SelectMany(i => Enumerable.Range(i + 1, a[0] - i - 1).Select(j => new { i, j }))
             .Count(_ =>
             {
-                var d = Math.Sqrt(Range(0, a[1]).Sum(k => Math.Pow(ps[_.i][k] - ps[_.j][k], 2)));
+                var d = Math.Sqrt(Enumerable.Range(0, a[1]).Sum(k => Math.Pow(ps[_.i][k] - ps[_.j][k], 2)));
                 return d == (int)d;
             });
         Console.WriteLine(c);
