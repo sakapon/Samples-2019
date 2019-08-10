019706475997441a417bd4dca0fe69a55ea1dd22
Keiho Sakapon <git@saka-pon.net>
2019-08-10 23:07:51 +0900

    Timeout again

--- AtCoderSample/ABC137/D.cs
+++ AtCoderSample/ABC137/D.cs
@@ -6,16 +6,18 @@ class D
 	static void Main()
 	{
 		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
-		var js = Enumerable.Range(0, a[0]).Select(i => new { i, j = Console.ReadLine().Split().Select(int.Parse).ToArray() }).OrderByDescending(_ => _.j[1]).ToDictionary(_ => _.i, _ => _.j);
+		var js = Enumerable.Range(0, a[0]).Select(i => Console.ReadLine().Split().Select(int.Parse).ToArray()).Where(j => j[0] <= a[1]).GroupBy(j => j[1]).OrderByDescending(g => g.Key).Select(g => g.OrderBy(j => j[0]).ToList()).ToList();
 
-		var g = 0L;
+		var r = 0L;
 		for (var i = 1; i <= a[1]; i++)
-		{
-			var o = js.FirstOrDefault(_ => _.Value[0] <= i);
-			if (o.Value == null) continue;
-			g += o.Value[1];
-			js.Remove(o.Key);
-		}
-		Console.WriteLine(g);
+			for (var k = 0; k < js.Count; k++)
+			{
+				if (js[k][0][0] > i) continue;
+				r += js[k][0][1];
+				js[k].RemoveAt(0);
+				if (js[k].Count == 0) js.RemoveAt(k);
+				break;
+			}
+		Console.WriteLine(r);
 	}
 }
