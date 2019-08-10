d3a525aa12ed80a217a21cbd497fe463427703e9
Keiho Sakapon <git@saka-pon.net>
2019-08-10 17:12:27 +0900

    Solve task

--- AtCoderSample/ABC133Console/C.cs
+++ AtCoderSample/ABC133Console/C.cs
@@ -3,10 +3,18 @@ using System.Linq;
 
 class C
 {
-	static void Main()
-	{
-		Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-	}
+    static void Main()
+    {
+        var a = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+
+        var m = long.MaxValue;
+        for (var i = a[0]; i < a[1]; i++)
+            for (var j = i + 1; j <= a[1]; j++)
+            {
+                var x = i * j % 2019;
+                if (x == 0) { Console.WriteLine(0); return; }
+                if (x < m) m = x;
+            }
+        Console.WriteLine(m);
+    }
 }
