a5f83a276bea61949e51588156c22ae636174368
Keiho Sakapon <git@saka-pon.net>
2019-08-10 22:15:32 +0900

    Time limit error

--- AtCoderSample/ABC137/D.cs
+++ AtCoderSample/ABC137/D.cs
@@ -5,8 +5,17 @@ class D
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
 		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+		var js = Enumerable.Range(0, a[0]).Select(i => new { i, j = Console.ReadLine().Split().Select(int.Parse).ToArray() }).OrderByDescending(_ => _.j[1]).ToDictionary(_ => _.i, _ => _.j);
+
+		var g = 0L;
+		for (var i = 1; i <= a[1]; i++)
+		{
+			var o = js.FirstOrDefault(_ => _.Value[0] <= i);
+			if (o.Value == null) continue;
+			g += o.Value[1];
+			js.Remove(o.Key);
+		}
+		Console.WriteLine(g);
 	}
 }
