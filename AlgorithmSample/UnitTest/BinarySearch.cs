7a673df993eb1aa75084dadf2f552638d97bcba6
Keiho Sakapon <git@saka-pon.net>
2019-12-18 16:31:44 +0900

    Add classes for binary search

---
+++ AlgorithmSample/UnitTest/BinarySearch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnitTest
+{
+	public static class BinarySearch
+	{
+		// 条件を満たす先頭の番号を探索します。
+		// f に r は渡されません。
+		static int Search(Func<int, bool> f, int l, int r)
+		{
+			int m;
+			while (l < r) if (f(m = (l + r - 1) / 2)) r = m; else l = m + 1;
+			return r;
+		}
+
+		// 条件を満たす最後の番号を探索します。
+		// f に l は渡されません。
+		static int SearchLast(Func<int, bool> f, int l, int r)
+		{
+			int m;
+			while (l < r) if (f(m = (l + r + 1) / 2)) l = m; else r = m - 1;
+			return r;
+		}
+
+		// 挿入先の番号を求めます。値が重複する場合は最後尾に挿入するときの番号です。すべて正の値です。
+		static int IndexForInsert(IList<int> a, int v) => Search(i => a[i] > v, 0, a.Count);
+
+		// Array.BinarySearch メソッドと異なる点: 一致する値が複数存在する場合は先頭の番号。
+		static int Index(IList<int> a, int v)
+		{
+			var r = Search(i => a[i] >= v, 0, a.Count);
+			return r < a.Count && a[r] == v ? r : ~r;
+		}
+
+		// Array.BinarySearch メソッドと異なる点: 一致する値が複数存在する場合は最後の番号。
+		static int IndexLast(IList<int> a, int v)
+		{
+			var r = SearchLast(i => a[i] <= v, -1, a.Count - 1);
+			return r >= 0 && a[r] == v ? r : ~(r + 1);
+		}
+	}
+}
---
+++ AlgorithmSample/UnitTest/BinarySearch0.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnitTest
+{
+	public static class BinarySearch0
+	{
+		// 挿入先の番号を求めます。値が重複する場合は最後尾です。すべて正の値です。
+		public static int IndexForInsert(IList<int> a, int v)
+		{
+			int l = 0, r = a.Count, m;
+			while (l < r)
+			{
+				m = (l + r - 1) / 2;
+				if (a[m] > v) r = m;
+				else l = m + 1;
+			}
+			return r;
+		}
+
+		// Array.BinarySearch メソッドと異なる点: 一致する値が複数存在する場合は先頭の番号。
+		public static int IndexOf(IList<int> a, int v)
+		{
+			int l = 0, r = a.Count, m;
+			while (l < r)
+			{
+				m = (l + r - 1) / 2;
+				if (a[m] >= v) r = m;
+				else l = m + 1;
+			}
+			return r < a.Count && a[r] == v ? r : ~r;
+		}
+	}
+}
