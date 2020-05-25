264315c8cf583677c78a35cebe96efb3f083acbd
Keiho Sakapon <git@saka-pon.net>
2020-05-25 13:24:12 +0900

    Other solution

---
+++ MathSample/UnitTest/CubicEquation6.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using static System.Math;
+
+namespace UnitTest
+{
+	public static class CubicEquation6
+	{
+		// (別解) 最小の実数解を求め、残りの二次方程式を解きます。
+		public static double[] Solve(double c, double d)
+		{
+			var f = CubicEquation5.CreateFunction(c, d);
+			var f1 = CubicEquation5.CreateDerivative(c);
+
+			if (d < 0) return Solve(c, -d).Reverse().Select(x => -x).ToArray();
+			// 3重解の場合 (c = d = 0)、f'(0) = 0 のため Newton 法を避けます。
+			if (c == 0 && d == 0) return new[] { 0.0 };
+
+			// 最小の実数解
+			var x1 = SolveMin();
+			// f(x) = (x - x_1) (x^2 + x_1 x + q)
+			// q = x_1^2 + c;
+			var det = -3 * x1 * x1 - 4 * c;
+			if (det < 0) return new[] { x1 };
+			if (det == 0) return new[] { x1, -x1 / 2 };
+			return new[] { x1, (-x1 - Sqrt(det)) / 2, (-x1 + Sqrt(det)) / 2 };
+
+			double SolveMin()
+			{
+				var x0 = -1.0;
+				while (f(x0) > 0) x0 *= 2;
+				return CubicEquation5.SolveByNewtonMethod(f, f1, x0);
+			}
+		}
+	}
+}
---
+++ MathSample/UnitTest/CubicEquationTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using KLibrary.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+	[TestClass]
+	public class CubicEquationTest
+	{
+		readonly Func<double, double, double[]> target = CubicEquation5.Solve;
+
+		[TestMethod]
+		public void Solve_1()
+		{
+			void Test(double c, double d)
+			{
+				var actual = target(c, d);
+				var f = CubicEquation5.CreateFunction(c, d);
+				foreach (var x in actual)
+					Assert2.AreNearlyEqual(0, f(x));
+			}
+
+			for (int c = -100; c <= 100; c++)
+				for (int d = 0; d <= 100; d++)
+					Test(c, d);
+		}
+
+		[TestMethod]
+		public void Solve_2()
+		{
+			void Test(double c, double d)
+			{
+				var actual = target(c, d);
+				var f = CubicEquation5.CreateFunction(c, d);
+				foreach (var x in actual)
+					Assert2.AreNearlyEqual(0, f(x));
+			}
+
+			Test(0, 2);
+			Test(0, -100);
+			Test(3, 0);
+			Test(1, 1);
+			Test(10, -10);
+			Test(-7, 0);
+			Test(-10, 127);
+			Test(-1, -10);
+			Test(-1.5, Math.Sqrt(2) / 2);
+			Test(-6 * Math.Pow(2, 1 / 3.0), -8);
+			Test(-100, 90);
+			Test(-15, -4);
+		}
+	}
+}
