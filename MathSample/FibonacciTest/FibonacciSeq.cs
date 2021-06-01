0f444cc3a810bdad1a3998f929a3b35007d27fc4
Keiho Sakapon <git@saka-pon.net>
2021-06-01 18:59:04 +0900

    Add FibonacciSeq class

---
+++ MathSample/FibonacciTest/FibonacciSeq.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Math;
+
+namespace FibonacciTest
+{
+	public static class FibonacciSeq
+	{
+		static readonly double Sqrt5 = Sqrt(5);
+		static readonly double φ = (1 + Sqrt5) / 2;
+		static readonly double φ_ = (1 - Sqrt5) / 2;
+
+		// F_92 は Int64 の範囲内です。
+		public static long[] Create(int max = 92)
+		{
+			var a = new long[max + 1];
+			a[1] = 1;
+			for (int i = 2; i <= max; i++)
+				a[i] = a[i - 1] + a[i - 2];
+			return a;
+		}
+
+		public static long GetValue(int n)
+		{
+			var v = (Pow(φ, n) - Pow(φ_, n)) * Sqrt5 / 5;
+			return (long)Round(v);
+		}
+	}
+}
---
+++ MathSample/FibonacciTest/FibonacciSeqTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FibonacciTest
+{
+	[TestClass]
+	public class FibonacciSeqTest
+	{
+		[TestMethod]
+		public void Create()
+		{
+			var expected = new[] { 0L, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 };
+			var f = FibonacciSeq.Create();
+
+			CollectionAssert.AreEqual(expected, f[..13]);
+		}
+
+		[TestMethod]
+		public void GetValue()
+		{
+			var f = FibonacciSeq.Create();
+
+			for (int i = 0; i <= 70; i++)
+				Assert.AreEqual(f[i], FibonacciSeq.GetValue(i));
+
+			Assert.AreNotEqual(f[71], FibonacciSeq.GetValue(71));
+		}
+	}
+}
