755bc6297c768fe282b1d0f42ff9be02ed62a110
Keiho Sakapon <git@saka-pon.net>
2021-06-01 16:06:30 +0900

    Create test project

---
+++ MathSample/FibonacciTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FibonacciTest
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
