b17aec52e1e735cf33591ed3c077060afefbb90f
Keiho Sakapon <git@saka-pon.net>
2021-06-09 11:24:49 +0900

    Add class for Lucas numbers

---
+++ MathSample/FibonacciTest/LucasNumbers.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using static System.Math;
+
+namespace FibonacciTest
+{
+	public static class LucasNumbers
+	{
+		public const int N_MaxForInt64 = 90;
+
+		// L_90 までは Int64 の範囲内です。
+		public static long[] CreateSeq(int nLast = N_MaxForInt64)
+		{
+			var a = new long[nLast + 1];
+			a[0] = 2;
+			a[1] = 1;
+			for (int i = 2; i <= nLast; i++)
+				a[i] = a[i - 1] + a[i - 2];
+			return a;
+		}
+
+		// O(n)
+		public static long[] CreateSeqWithMod(int nLast, long mod)
+		{
+			var a = new long[nLast + 1];
+			a[0] = 2;
+			a[1] = 1;
+			for (int i = 2; i <= nLast; i++)
+				a[i] = (a[i - 1] + a[i - 2]) % mod;
+			return a;
+		}
+
+		// O(log n)
+		public static long GetValueWithMod(int n, long mod)
+		{
+			var m = new ModMatrixOperator(mod);
+
+			var a = new long[2, 2];
+			a[0, 0] = a[0, 1] = a[1, 0] = 1;
+			a = m.Pow(a, n);
+			var v = m.Mul(a, new[] { 1L, 2 });
+			return v[1];
+		}
+
+		// 以下、実用性はほぼありません。
+		#region Lab
+
+		static readonly double Sqrt5 = Sqrt(5);
+		static readonly double φ = (1 + Sqrt5) / 2;
+		static readonly double φ_ = (1 - Sqrt5) / 2;
+
+		internal static long GetValueByGeneral(int n)
+		{
+			var v = Pow(φ, n) + Pow(φ_, n);
+			return (long)Round(v);
+		}
+
+		// 第 1 項のみ
+		internal static long GetValueByApprox(int n)
+		{
+			var v = GetRawValueByApprox(n);
+			return (long)Round(v);
+		}
+
+		internal static double GetRawValueByApprox(int n)
+		{
+			return Pow(φ, n);
+		}
+		#endregion
+	}
+}
---
+++ MathSample/FibonacciTest/LucasNumbersTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FibonacciTest
+{
+	[TestClass]
+	public class LucasNumbersTest
+	{
+		const int N_MaxForInt64 = LucasNumbers.N_MaxForInt64;
+		const long M = 1000000007;
+
+		[TestMethod]
+		public void CreateSeq()
+		{
+			var expected = new[] { 2L, 1, 3, 4, 7, 11, 18, 29, 47, 76, 123, 199 };
+			var f = LucasNumbers.CreateSeq();
+
+			CollectionAssert.AreEqual(expected, f[..expected.Length]);
+		}
+
+		[TestMethod]
+		public void CreateSeqWithMod()
+		{
+			// L_n < M となる範囲
+			var nLast = 43;
+			var f = LucasNumbers.CreateSeq(nLast);
+			var fm = LucasNumbers.CreateSeqWithMod(nLast, M);
+
+			CollectionAssert.AreEqual(f, fm);
+		}
+
+		[TestMethod]
+		public void GetValueWithMod()
+		{
+			var nLast = 100000;
+			var fm = LucasNumbers.CreateSeqWithMod(nLast, M);
+
+			for (int i = 0; i <= nLast; i++)
+				Assert.AreEqual(fm[i], LucasNumbers.GetValueWithMod(i, M));
+		}
+
+		[TestMethod]
+		public void GetValueByGeneral()
+		{
+			var f = LucasNumbers.CreateSeq();
+
+			for (int i = 0; i <= 68; i++)
+				Assert.AreEqual(f[i], LucasNumbers.GetValueByGeneral(i));
+
+			for (int i = 69; i <= N_MaxForInt64; i++)
+				Assert.AreNotEqual(f[i], LucasNumbers.GetValueByGeneral(i));
+		}
+
+		[TestMethod]
+		public void GetValueByApprox()
+		{
+			var f = LucasNumbers.CreateSeq();
+
+			// i < 2 は省略
+			for (int i = 2; i <= 68; i++)
+				Assert.AreEqual(f[i], LucasNumbers.GetValueByApprox(i));
+
+			for (int i = 69; i <= N_MaxForInt64; i++)
+				Assert.AreNotEqual(f[i], LucasNumbers.GetValueByApprox(i));
+		}
+
+		[TestMethod]
+		public void Display_GetRawValueByApprox()
+		{
+			var f = LucasNumbers.CreateSeq();
+
+			for (var i = 0; i <= N_MaxForInt64; i++)
+				Console.WriteLine($"φ^{i} = {LucasNumbers.GetRawValueByApprox(i)} ≒ {f[i]}");
+		}
+	}
+}
