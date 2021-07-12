eb18d57f5fbe233bbb79fc33cdd7e91faa0b8776
Keiho Sakapon <git@saka-pon.net>
2021-07-12 15:09:18 +0900

    Add LucasSeq3 class

---
+++ MathSample/FibonacciTest/LucasSeq3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FibonacciTest
+{
+	// a_{n+3} = p * a_{n+2} - q * a_{n+1} + r * a_n
+	public static class LucasSeq3
+	{
+		static long MInt(long x, long mod) => (x %= mod) < 0 ? x + mod : x;
+
+		// O(n)
+		public static long[] CreateSeqWithMod(long p, long q, long r, long a0, long a1, long a2, int nLast, long mod)
+		{
+			var a = new long[nLast + 1];
+			a[0] = a0;
+			a[1] = a1;
+			a[2] = a2;
+			for (int i = 3; i <= nLast; i++)
+				a[i] = MInt(p * a[i - 1] - q * a[i - 2] + r * a[i - 3], mod);
+			return a;
+		}
+
+		// O(log n)
+		public static long GetValueWithMod(long p, long q, long r, long a0, long a1, long a2, long n, long mod)
+		{
+			var m = new ModMatrixOperator(mod);
+
+			var a = new long[3, 3];
+			a[0, 0] = p;
+			a[0, 1] = -q;
+			a[0, 2] = r;
+			a[1, 0] = a[2, 1] = 1;
+			a = m.Pow(a, n);
+			var v = m.Mul(a, new[] { a2, a1, a0 });
+			return v[2];
+		}
+	}
+}
---
+++ MathSample/FibonacciTest/LucasSeq3Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FibonacciTest
+{
+	[TestClass]
+	public class LucasSeq3Test
+	{
+		const long M = 1000000007;
+
+		[TestMethod]
+		public void CreateSeqWithMod()
+		{
+			var nLast = 33;
+			var expected = new[] { 0L, 1, 1, 1, 2, 5, 12, 28, 65, 151 };
+			var am = LucasSeq3.CreateSeqWithMod(3, 2, 1, 0, 1, 1, nLast, M);
+
+			CollectionAssert.AreEqual(expected, am[..expected.Length]);
+		}
+
+		[TestMethod]
+		public void GetValueWithMod()
+		{
+			var nLast = 20000;
+			var am = LucasSeq3.CreateSeqWithMod(3, 2, 1, 0, 1, 1, nLast, M);
+
+			for (int i = 0; i <= nLast; i++)
+				Assert.AreEqual(am[i], LucasSeq3.GetValueWithMod(3, 2, 1, 0, 1, 1, i, M));
+		}
+	}
+}
