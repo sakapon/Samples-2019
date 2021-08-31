5c37b62deaa2882fddbff57445d3257be459a5b2
Keiho Sakapon <git@saka-pon.net>
2021-08-31 16:29:59 +0900

    Create test project

---
+++ MathSample/DftNttTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DftNttTest
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
