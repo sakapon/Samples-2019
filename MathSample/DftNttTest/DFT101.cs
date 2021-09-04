882f74783d3999cd4ba7e0a391939a7f93596126
Keiho Sakapon <git@saka-pon.net>
2021-09-04 18:23:35 +0900

    Add function

--- MathSample/DftNttTest/DFT101.cs
+++ MathSample/DftNttTest/DFT101.cs
@@ -5,7 +5,7 @@ namespace DftNttTest
 {
 	public static class DFT101
 	{
-		// k-th "n-th root" of 1
+		// k 番目の 1 の n 乗根
 		static Complex NthRoot(int n, int k)
 		{
 			var t = 2 * Math.PI * k / n;
@@ -28,6 +28,25 @@ namespace DftNttTest
 				if (inverse) r[k] /= n;
 			}
 			return r;
+		}
+
+		public static Complex[] Convolution(Complex[] a, Complex[] b)
+		{
+			if (a == null) throw new ArgumentNullException(nameof(a));
+			if (b == null) throw new ArgumentNullException(nameof(b));
+
+			var n = a.Length + b.Length - 1;
+			Array.Resize(ref a, n);
+			Array.Resize(ref b, n);
+
+			var fa = Transform(a, false);
+			var fb = Transform(b, false);
+
+			for (int k = 0; k < n; ++k)
+			{
+				fa[k] *= fb[k];
+			}
+			return Transform(fa, true);
 		}
 	}
 }
---
+++ MathSample/DftNttTest/DFT101Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Numerics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DftNttTest
+{
+	[TestClass]
+	public class DFT101Test
+	{
+		[TestMethod]
+		public void Transform()
+		{
+			var c1 = new Complex[] { 3, 4, 5, 6, 7 };
+			var t = DFT101.Transform(c1, false);
+			var c2 = DFT101.Transform(t, true);
+			CollectionAssert.AreEqual(c2.ToLong(), c1.ToLong());
+		}
+
+		[TestMethod]
+		public void Convolution()
+		{
+			var a = new Complex[] { 1, 2, 3, 4 };
+			var b = new Complex[] { 5, 6, 7, 8, 9 };
+			var expected = new long[] { 5, 16, 34, 60, 70, 70, 59, 36 };
+			var actual = DFT101.Convolution(a, b);
+			CollectionAssert.AreEqual(expected, actual.ToLong());
+		}
+	}
+}
