64b7e8cc511a44d336930f37ec413ade34e3dc68
Keiho Sakapon <git@saka-pon.net>
2021-09-05 22:53:22 +0900

    Add class for FFT

---
+++ MathSample/DftNttTest/FFT101.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Numerics;
+
+namespace DftNttTest
+{
+	public static class FFT101
+	{
+		public static int ToPowerOf2(int n)
+		{
+			var p = 1;
+			while (p < n) p <<= 1;
+			return p;
+		}
+
+		// k 番目の 1 の n 乗根
+		static Complex NthRoot(int n, int k)
+		{
+			var t = 2 * Math.PI * k / n;
+			return Complex.FromPolarCoordinates(1, t);
+		}
+
+		// n = c.Length は 2 の冪としてください。
+		static void TransformRecIn(Complex[] c, bool inverse)
+		{
+			var n = c.Length;
+			if (n == 1) return;
+
+			var n2 = n >> 1;
+			var c0 = new Complex[n2];
+			var c1 = new Complex[n2];
+			for (int k = 0; k < n2; ++k)
+			{
+				c0[k] = c[2 * k];
+				c1[k] = c[2 * k + 1];
+			}
+
+			TransformRecIn(c0, inverse);
+			TransformRecIn(c1, inverse);
+
+			for (int k = 0; k < n2; ++k)
+			{
+				var z = c1[k] * NthRoot(n, inverse ? -k : k);
+				c[k] = c0[k] + z;
+				c[k + n2] = c0[k] - z;
+			}
+		}
+
+		// 戻り値の長さは 2 の冪となります。
+		public static Complex[] TransformRec(Complex[] c, bool inverse)
+		{
+			if (c == null) throw new ArgumentNullException(nameof(c));
+
+			var n = ToPowerOf2(c.Length);
+			var t = new Complex[n];
+			c.CopyTo(t, 0);
+			TransformRecIn(t, inverse);
+			if (inverse) for (int k = 0; k < n; ++k) t[k] /= n;
+			return t;
+		}
+
+		// 戻り値の長さは 2 の冪となります。
+		public static Complex[] Convolution(Complex[] a, Complex[] b)
+		{
+			if (a == null) throw new ArgumentNullException(nameof(a));
+			if (b == null) throw new ArgumentNullException(nameof(b));
+
+			var n = ToPowerOf2(a.Length + b.Length - 1);
+			Array.Resize(ref a, n);
+			Array.Resize(ref b, n);
+
+			var fa = TransformRec(a, false);
+			var fb = TransformRec(b, false);
+
+			for (int k = 0; k < n; ++k)
+			{
+				fa[k] *= fb[k];
+			}
+			return TransformRec(fa, true);
+		}
+	}
+}
---
+++ MathSample/DftNttTest/FFT101Test.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DftNttTest
+{
+	[TestClass]
+	public class FFT101Test
+	{
+		[TestMethod]
+		public void TransformRec()
+		{
+			var c1 = new Complex[] { 3, 4, 5, 6, 7 };
+			var t = FFT101.TransformRec(c1, false);
+			var c2 = FFT101.TransformRec(t, true);
+			Array.Resize(ref c1, FFT101.ToPowerOf2(c1.Length));
+			CollectionAssert.AreEqual(c2.ToLong(), c1.ToLong());
+		}
+
+		[TestMethod]
+		public void TransformRec_Many()
+		{
+			var n = 1 << 16;
+			var a = Enumerable.Range(3, n).ToArray();
+			var c1 = Array.ConvertAll(a, x => new Complex(x, 0));
+			var t = FFT101.TransformRec(c1, false);
+			var c2 = FFT101.TransformRec(t, true);
+			CollectionAssert.AreEqual(c2.ToLong(), c1.ToLong());
+		}
+
+		[TestMethod]
+		public void Convolution()
+		{
+			var a = new Complex[] { 1, 2, 3, 4 };
+			var b = new Complex[] { 5, 6, 7, 8, 9 };
+			var expected = new long[] { 5, 16, 34, 60, 70, 70, 59, 36 };
+			var actual = FFT101.Convolution(a, b);
+			CollectionAssert.AreEqual(expected, actual.ToLong());
+		}
+	}
+}
