922248739d59dfe12cedc6ac46b6569cf210588d
Keiho Sakapon <git@saka-pon.net>
2021-09-09 19:50:34 +0900

    Update test

--- MathSample/DftNttTest/Tests/ConvolutionNaiveTest.cs
+++ MathSample/DftNttTest/Tests/ConvolutionNaiveTest.cs
@@ -7,10 +7,9 @@ namespace DftNttTest.Tests
 	[TestClass]
 	public class ConvolutionNaiveTest
 	{
-		const int n1 = 1 << 10;
-		const int n2 = 1 << 16;
+		const int n = 1 << 10;
 
-		static void Test(int n, Func<long[], long[], long[]> convolution)
+		static void Test(Func<long[], long[], long[]> convolution)
 		{
 			var f = Enumerable.Range(3, n).Select(v => (long)v).ToArray();
 			var g = Enumerable.Range(-5, n).Select(v => (long)v).ToArray();
@@ -21,37 +20,19 @@ namespace DftNttTest.Tests
 		[TestMethod]
 		public void Convolution_DFT()
 		{
-			Test(n1, DFT.Convolution);
+			Test(DFT.Convolution);
 		}
 
 		[TestMethod]
 		public void Convolution_DFT101()
 		{
-			Test(n1, (f, g) => DFT101.Convolution(f.ToComplex(), g.ToComplex()).ToInt64());
+			Test((f, g) => DFT101.Convolution(f.ToComplex(), g.ToComplex()).ToInt64());
 		}
 
 		[TestMethod]
 		public void Convolution_DFT102()
 		{
-			Test(n1, (f, g) => DFT102.Convolution(f.ToComplex(), g.ToComplex()).ToInt64());
-		}
-
-		[TestMethod]
-		public void Convolution_FFT()
-		{
-			Test(n2, FFT.Convolution);
-		}
-
-		[TestMethod]
-		public void Convolution_FFT101()
-		{
-			Test(n2, (f, g) => FFT101.Convolution(f.ToComplex(), g.ToComplex()).ToInt64().Resize(f.Length + g.Length - 1));
-		}
-
-		[TestMethod]
-		public void Convolution_FFT102()
-		{
-			Test(n2, (f, g) => FFT102.Convolution(f.ToComplex(), g.ToComplex()).ToInt64().Resize(f.Length + g.Length - 1));
+			Test((f, g) => DFT102.Convolution(f.ToComplex(), g.ToComplex()).ToInt64());
 		}
 	}
 }
