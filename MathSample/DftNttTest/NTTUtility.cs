6475f18aeab888724d4b3969f0dc507f0aa38d73
Keiho Sakapon <git@saka-pon.net>
2021-09-16 11:44:48 +0900

    Add NTTUtility class

---
+++ MathSample/DftNttTest/NTTUtility.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DftNttTest
+{
+	public static class NTTUtility
+	{
+		static bool IsPrime(long n)
+		{
+			for (long x = 2; x * x <= n; ++x) if (n % x == 0) return false;
+			return n > 1;
+		}
+
+		// p = dn + 1 が素数となる (p, d)
+		public static (long p, long d) FindMinPrime(long n)
+		{
+			for (long d = 1; ; ++d)
+			{
+				var p = d * n + 1;
+				if (IsPrime(p)) return (p, d);
+			}
+		}
+
+		// p = dn + 1 が素数となる (p, d)
+		public static IEnumerable<(long p, long d)> FindPrimes(long n, long min, long max)
+		{
+			var d = Math.Max(1, (min - 2) / n + 1);
+			var p = d * n + 1;
+			for (; p <= max; ++d, p += n)
+			{
+				if (IsPrime(p)) yield return (p, d);
+			}
+		}
+	}
+}
---
+++ MathSample/DftNttTest/Tests/NTTUtilityTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DftNttTest.Tests
+{
+	[TestClass]
+	public class NTTUtilityTest
+	{
+		[TestMethod]
+		public void FindMinPrime()
+		{
+			for (int n = 1; n > 0; n <<= 1)
+			{
+				var (p, d) = NTTUtility.FindMinPrime(n);
+				Console.WriteLine($"n = {n}: p = {p}, d = {d}");
+			}
+		}
+
+		[TestMethod]
+		public void FindPrimes()
+		{
+			var n = 1 << 23;
+			Console.WriteLine($"n = {n}");
+			foreach (var (p, d) in NTTUtility.FindPrimes(n, 800000000, int.MaxValue))
+			{
+				Console.WriteLine($"p = {p}, d = {d}");
+			}
+		}
+	}
+}
