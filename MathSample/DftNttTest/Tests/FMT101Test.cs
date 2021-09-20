8552abe5cc681399667600ceabf472acb8b24912
Keiho Sakapon <git@saka-pon.net>
2021-09-20 09:53:10 +0900

    Add test

--- MathSample/DftNttTest/Tests/FMT101Test.cs
+++ MathSample/DftNttTest/Tests/FMT101Test.cs
@@ -5,14 +5,14 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace DftNttTest.Tests
 {
 	[TestClass]
-	public class NTT101Test
+	public class FMT101Test
 	{
 		static void Transform(int n, long m, long w, bool result)
 		{
-			var ntt = new NTT101(n, m, w);
+			var fmt = new FMT101(n, m, w);
 			var f1 = Enumerable.Range(3, 5).Select(v => (long)v).ToArray();
-			var f_ = ntt.Transform(f1, false);
-			var f2 = ntt.Transform(f_, true).Resize(f1.Length);
+			var f_ = fmt.Transform(f1, false);
+			var f2 = fmt.Transform(f_, true).Resize(f1.Length);
 
 			if (result) CollectionAssert.AreEqual(f1, f2);
 			else CollectionAssert.AreNotEqual(f1, f2);
@@ -20,11 +20,11 @@ namespace DftNttTest.Tests
 
 		static void Convolution(int n, long m, long w, bool result)
 		{
-			var ntt = new NTT101(n, m, w);
+			var fmt = new FMT101(n, m, w);
 			var f = new long[] { 2, 1, 1 };
 			var g = new long[] { m - 1, m - 1, 1 };
 			var expected = new long[] { m - 2, m - 3, 0, 0, 1 };
-			var actual = ntt.Convolution(f, g).Resize(expected.Length);
+			var actual = fmt.Convolution(f, g).Resize(expected.Length);
 
 			if (result) CollectionAssert.AreEqual(expected, actual);
 			else CollectionAssert.AreNotEqual(expected, actual);
@@ -39,19 +39,13 @@ namespace DftNttTest.Tests
 		[TestMethod]
 		public void TestAll()
 		{
-			Test(5, 31, 2, true);
-			Test(5, 25, 6, false);
-			Test(5, 55, 16, false);
-			Test(5, 22, 3, false);
-			Test(5, 121, 3, true);
-			Test(5, 341, 4, true);
+			Test(8, 17, 2, true);
+			Test(8, 51, 2, false);
+			Test(8, 289, 110, true);
 
-			Test(6, 31, 6, true);
-			Test(6, 21, 2, false);
-			Test(6, 21, 5, false);
-			Test(6, 35, 4, false);
-			Test(6, 49, 19, true);
-			Test(6, 91, 10, true);
+			Test(16, 17, 3, true);
+			Test(16, 51, 5, false);
+			Test(16, 289, 40, true);
 
 			var n = 1 << 8;
 			var w = MPow(g, (M - 1) / n);
