def9411ddba46dee3812c7589ed13610a2ec448f
Keiho Sakapon <git@saka-pon.net>
2021-09-22 14:20:20 +0900

    Add NTT101 class

---
+++ MathSample/DftNttTest/NTT101.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace DftNttTest
+{
+	public class NTT101
+	{
+		// k 番目の 1 の n 乗根 (ω^k)
+		static long[] NthRoots(int n, long m, long w)
+		{
+			var r = new long[n];
+			r[0] = 1;
+			for (int k = 1; k < n; ++k)
+				r[k] = r[k - 1] * w % m;
+			return r;
+		}
+
+		int n;
+		long m, nInv;
+		long[] roots;
+
+		public NTT101(int n, long m, long w, long nInv)
+		{
+			this.n = n;
+			this.m = m;
+			this.nInv = nInv;
+			roots = NthRoots(n, m, w);
+		}
+
+		// f(ω^k) の値
+		long f(long[] c, int k)
+		{
+			var r = 0L;
+			for (int j = 0; j < c.Length; ++j)
+				r = (r + c[j] * roots[k * j % n]) % m;
+			return r;
+		}
+
+		public long[] Transform(long[] c, bool inverse)
+		{
+			if (c == null) throw new ArgumentNullException(nameof(c));
+
+			var r = new long[n];
+			for (int k = 0; k < n; ++k)
+				r[k] = inverse ? f(c, n - k) * nInv % m : f(c, k);
+			return r;
+		}
+
+		public long[] Convolution(long[] a, long[] b)
+		{
+			if (a == null) throw new ArgumentNullException(nameof(a));
+			if (b == null) throw new ArgumentNullException(nameof(b));
+
+			var fa = Transform(a, false);
+			var fb = Transform(b, false);
+
+			for (int k = 0; k < n; ++k)
+			{
+				fa[k] = fa[k] * fb[k] % m;
+			}
+			return Transform(fa, true);
+		}
+	}
+}
---
+++ MathSample/DftNttTest/Tests/NTT101Test.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DftNttTest.Tests
+{
+	[TestClass]
+	public class NTT101Test
+	{
+		static void Transform(int n, long m, long w, long nInv)
+		{
+			var ntt = new NTT101(n, m, w, nInv);
+			var f1 = Enumerable.Range(3, 5).Select(v => (long)v).ToArray();
+			var f_ = ntt.Transform(f1, false);
+			var f2 = ntt.Transform(f_, true).Resize(f1.Length);
+
+			CollectionAssert.AreEqual(f1, f2);
+		}
+
+		static void Convolution(int n, long m, long w, long nInv)
+		{
+			var ntt = new NTT101(n, m, w, nInv);
+			var f = new long[] { 2, 1, 1 };
+			var g = new long[] { m - 1, m - 1, 1 };
+			var expected = new long[] { m - 2, m - 3, 0, 0, 1 };
+			var actual = ntt.Convolution(f, g).Resize(expected.Length);
+
+			CollectionAssert.AreEqual(expected, actual);
+		}
+
+		static void Test(int n, long m, long w, long nInv)
+		{
+			Transform(n, m, w, nInv);
+			Convolution(n, m, w, nInv);
+		}
+
+		[TestMethod]
+		public void TestAll()
+		{
+			Test(5, 31, 2, 25);
+			Test(5, 121, 3, 97);
+			Test(5, 341, 4, 273);
+
+			Test(6, 31, 6, 26);
+			Test(6, 49, 19, 41);
+			Test(6, 91, 10, 76);
+
+			Test(8, 17, 2, 15);
+			Test(8, 289, 110, 253);
+		}
+	}
+}
