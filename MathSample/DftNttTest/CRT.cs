383a0894e381894512e6c679840c07e0cc5df612
Keiho Sakapon <git@saka-pon.net>
2021-09-29 14:51:21 +0900

    Add class for CRT

---
+++ MathSample/DftNttTest/CRT.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Numerics;
+
+namespace DftNttTest
+{
+	public class CRT
+	{
+		// ax + by = 1 の解
+		// 前提: a と b は互いに素
+		public static (long x, long y) ExtendedEuclid(long a, long b)
+		{
+			if (b == 0) throw new ArgumentOutOfRangeException(nameof(b));
+			if (b == 1) return (1, 1 - a);
+
+			var q = Math.DivRem(a, b, out var r);
+			var (x, y) = ExtendedEuclid(b, r);
+			return (y, x - q * y);
+		}
+
+		static BigInteger MInt(BigInteger x, long M) => (x %= M) < 0 ? x + M : x;
+
+		long m, n, mn;
+		long u, v;
+
+		// 前提: m と n は互いに素
+		public CRT(long m, long n)
+		{
+			this.m = m;
+			this.n = n;
+			mn = m * n;
+			(u, v) = ExtendedEuclid(m, n);
+		}
+
+		// a mod m かつ b mod n である値 (mod mn で唯一)
+		// 戻り値は anv + bmu
+		public long Solve(long a, long b)
+		{
+			var r = (BigInteger)a * n * v + (BigInteger)b * m * u;
+			return (long)MInt(r, mn);
+		}
+	}
+}
---
+++ MathSample/DftNttTest/Tests/CRTTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DftNttTest.Tests
+{
+	[TestClass]
+	public class CRTTest
+	{
+		[TestMethod]
+		public void Solve_1()
+		{
+			var crt1 = new CRT(3, 5);
+			var crt2 = new CRT(15, 7);
+
+			var x1 = crt1.Solve(2, 3);
+			var x2 = crt2.Solve(x1, 2);
+
+			Assert.AreEqual(23, x2);
+		}
+
+		[TestMethod]
+		public void Solve_2()
+		{
+			var expected = 1_000000000_000000000;
+			var p1 = 998244353;
+			var p2 = 1107296257;
+
+			var crt = new CRT(p1, p2);
+			var actual = crt.Solve(expected % p1, expected % p2);
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
