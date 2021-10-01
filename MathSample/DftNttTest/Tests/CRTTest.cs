f6991ba2120184f1372401fd97a7ead1c96d9f09
Keiho Sakapon <git@saka-pon.net>
2021-10-01 11:25:06 +0900

    Add test

--- MathSample/DftNttTest/Tests/CRTTest.cs
+++ MathSample/DftNttTest/Tests/CRTTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DftNttTest.Tests
@@ -28,6 +29,28 @@ namespace DftNttTest.Tests
 			var crt = new CRT(p1, p2);
 			var actual = crt.Solve(expected % p1, expected % p2);
 			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void Convolution()
+		{
+			const long p1 = 998244353, g1 = 3;
+			const long p2 = 1107296257, g2 = 10;
+
+			var fntt1 = new FNTT202(8, p1, g1);
+			var fntt2 = new FNTT202(8, p2, g2);
+
+			var a = new long[] { 1000000, 1000000, 1000000 };
+			var b = new long[] { 1000000, 1000000, 1000000, 1000000 };
+
+			var c1 = fntt1.Convolution(a, b);
+			var c2 = fntt2.Convolution(a, b);
+
+			var crt = new CRT(p1, p2);
+			var c = c1.Zip(c2, (x, y) => crt.Solve(x, y)).ToArray();
+
+			var expected = new long[] { 1000000000000, 2000000000000, 3000000000000, 3000000000000, 2000000000000, 1000000000000, 0, 0 };
+			CollectionAssert.AreEqual(expected, c);
 		}
 	}
 }
